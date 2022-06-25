e24ef6d98e54ba5f67c4452672b1422e63fb321d
zqr <2635869371@qq.com>
2022-06-25 20:03:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 张起荣
+ * @motto 我亦无他 唯手熟尔
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
